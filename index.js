fa293a8a1a08a3e413ed4d509ee68505cbaef6cf
fra.ascenzi <theouter.guy@gmail.com>
2023-11-14 12:20:19 +0000

    Try catch deleted | Return with console.error in case of error

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 'use strict';
 
-/** WERLOG 4.0.0
- *
- * Lightweigth error handling routines for Node
+/** WERLOG 4.0 | Lightweigth error handling routines for Node
  *
  * @param {String} message error message string
  * @param {Number} maxLength max string length (default = 3600)
  *
  * @author Francesco "Frash" Ascenzi
  *
- * @version 4.0.0
+ * @version 4.0
  * @license Apache 2.0
  */
-function werlog(message, maxLength = 3600) {
-    try {
-        if (typeof message != 'string' && message.trim() == '') {
-            throw new Error('Error is not a string or its value is empty');
-        }
+async function werlog(message, maxLength = 3600) {
+    // Convert it to a string and check for its value and its length
+    message = message.toString();
 
-        let fs = require('fs');
-
-        let mess = '';
-        if (message.length > maxLength) {
-            mess = message.substring(0, maxLength);
-        }
-
-        let dateFormat = new Date().toLocaleString();
-        let content = `[${dateFormat}]Error: ${mess}\r\n`;
-
-        fs.appendFile('werlog.txt', content, 'utf-8');
-    } catch (e) {
-        console.error(e);
+    if (message.trim() == '') {
+        console.error('Error string is empty');
+        return;
     }
+
+    if (message.length > maxLength) {
+        message = message.substring(0, maxLength);
+        return;
+    }
+
+    // Require the file system module and assign date and file text
+    let fs = require('fs');
+
+    let date = new Date().toLocaleString();
+    let text = `[${date}]${message}\r\n`;
+
+    // Create and append line on the last line
+    await fs.promises.appendFile('werlog.txt', text, 'utf-8', err => {
+        if (err) {
+            console.error("It may not be possible to create the 'werlog' file");
+            return;
+        }
+    });
 }
 
 module.exports = werlog;
