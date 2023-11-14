f02bba849626177cfdf767800490d4f46dcbed6a
fra.ascenzi <theouter.guy@gmail.com>
2023-11-14 11:42:09 +0000

    Version 4.0.0 relased

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-'use strict'
+'use strict';
 
-/** WERLOG 1.0.0
- * 
- * Lightweigth error handling routines for nodejs by Francesco "Frash" Ascenzi
- * 
- * @param {String} message string | Log error
- * @return {Txt} file | A file that contains the full error code
- * 
+var fs = require('fs');
+
+/** WERLOG 4.0.0
+ *
+ * Lightweigth error handling routines for Node
+ *
+ * @param {String} message error message string
+ * @param {Number} maxLength max string length (default = 3600)
+ *
  * @author Francesco "Frash" Ascenzi
- * 
- * @version 1.0.0
+ *
+ * @version 4.0.0
  * @license Apache 2.0
 **/
-function werlog(message) {
-    if (typeof message != 'string')
-        return console.error("Error text is not a string");
-
-    let mess = message;
-    if (message.length >= 3600) // Max string length
-        mess = message.substring(0, 3600);
-
-    let reqFile = require('fs');
-
-    let dateFormat = new Date().toLocaleString();
-    let content = `[${dateFormat}]Error: ${mess}` + '\r\n';
-    let encoding = 'utf8';
-
-    reqFile.appendFile('werlog.txt', content, encoding, err => {
-        if (err) {
-            console.error(err);
+function werlog(message, maxLength = 3600) {
+    try {
+        if (typeof message != 'string' && message.trim() == '') {
+            throw new Error('Error is not a string or its value is empty');
         }
-    });
+
+        var mess = '';
+        if (message.length > maxLength) {
+            mess = message.substring(0, maxLength);
+        }
+
+        var dateFormat = new Date().toLocaleString();
+        var content = "[".concat(dateFormat, "]Error: ").concat(mess) + '\r\n';
+
+        fs.appendFile('werlog.txt', content, 'utf-8');
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 module.exports = werlog;
